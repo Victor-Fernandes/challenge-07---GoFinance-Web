e84dabd7d49f11c03b27333e1364f29982d5b2ed
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-05-08 16:44:50 -0300

    finalizando upload csv

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -102,11 +102,10 @@ const Dashboard: React.FC = () => {
 
             <tbody>
               {transactions.map(transaction => (
-                <tr>
+                <tr key={transaction.id}>
                   <td className="title">{transaction.title}</td>
                   <td className={transaction.type}>
-                    {/* Melhorar aqui,   */}
-                    {transaction.type === 'outcome' && '-'}
+                    {transaction.type === 'outcome' && '- '}
                     {transaction.formattedValue}
                   </td>
                   <td>{transaction.category.title}</td>
